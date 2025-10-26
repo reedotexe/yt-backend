c855424982632377aede76258d974786f3f4293f
reereereereeree <reereeree369@gmail.com>
2025-10-27 04:25:07 +0530

    database

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+// const MONGO_URI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+
+    // console.log("MONGO_URI:", process.env.MONGO_URI);
+};
+
+export default connectDB;
